68dda16b2bacfbb45e1f36b2eefa00fd2218399c
Gustavo Rodrigues <grodriguers@gmail.com>
2022-07-06 13:57:10 -0300

    feat: create task component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import Tasks from "./components/Tasks";
+import './App.css';
 
 const App = () => {
-  const message = 'Test'
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Estudar',
+      completed: false,
+    },
+    {
+      id: '2',
+      title: 'Ler',
+      completed: true,
+    }
+  ]);
+
+  //setTasks([])
   return (
-    <h1> { message } </h1>
+    <>
+      <div className="container">
+        <Tasks tasks={tasks} />
+      </div>
+      
+    
+    </>
+    
   )
-}
+};
 export default App;
---
+++ src/components/Tasks.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tasks = ({ tasks }) => {
+    console.log(tasks);
+    return <h1> Tasks </h1>;
+};
+export default Tasks;
